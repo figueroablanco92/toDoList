5d95f850cf0b4ef43a8f985c331cc01c44fd9bc0
nicoya505 <figueroablanco92@gmail.com>
2023-08-24 00:36:24 +0200

    refactor(login): update data and add new method to layer login

--- src/controller/ControladorLogin.js
+++ src/controller/ControladorLogin.js
@@ -1,18 +1,13 @@
-import User from "../model/User.js";
-import LoginConsultas from "../model/LoginConsultas.js";
+import User from "../model/User.js"
+import LoginConsultas from "../model/LoginConsultas.js"
 
-const loginForm = document.getElementById("login-form");
-const txtUser = document.getElementById("txtUser");
-const txtPass = document.getElementById("txtPass");
+const loginForm = document.getElementById("login-form")
+const txtUser = document.getElementById("txtUser")
+const txtPass = document.getElementById("txtPass")
 
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  LoginConsultas.verificarUsuario(new User( txtUser.value,txtPass.value))
+})
 
-  let name = txtUser.value;
-  let pass = txtPass.value;
 
-  LoginConsultas.verificarUsuario(new User( name, pass));
-});
-
-// const user = new User("alex","123")
-// LoginConsultas.verificarUsuario(user)
